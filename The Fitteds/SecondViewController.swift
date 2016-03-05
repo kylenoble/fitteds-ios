30ec61c04fd1f212f7579a5a06f59fab16b7f39d
Kyle Noble <kyle@531networks.com>
2016-03-05 11:17:14 -0500

    Initial Commit

---
+++ The Fitteds/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  The Fitteds
+//
+//  Created by Kyle Noble on 3/5/16.
+//  Copyright © 2016 Kyle Noble. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
